4f7bf04e8c86623d555f36218abfde20234c3f33
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-07-24 16:41:34 +0100

    Sidebar Porject

--- index.js
+++ index.js
@@ -10,12 +10,23 @@
 // });
 
 const hamburger = document.getElementById("hamburger");
-const navLink = document.getElementById("navLink");
+const sidebar = document.getElementById("sidebar");
+const exit = document.getElementById("exit");
 
 hamburger.addEventListener("click", function () {
-  if (navLink.classList.contains("show")) {
-    navLink.classList.remove("show");
-  } else {
-    navLink.classList.add("show");
-  }
+  sidebar.classList.toggle("open");
+  this.classList.toggle("rotate");
 });
+
+exit.addEventListener("click", function () {
+  sidebar.classList.toggle("open");
+  this.classList.toggle("rotate");
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const hamburger = document.getElementById("hamburger");
+
+//   hamburger.addEventListener("click", function () {
+//     this.classList.toggle("rotate");
+//   });
+// });
